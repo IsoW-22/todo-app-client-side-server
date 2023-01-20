4a1de83bef7eff438f62f0b6eabcd1c48aca7a6c
IsoW-22 <isowz2000@gmail.com>
2023-01-20 17:54:17 +0330

    added date.js, removed date function from html

---
+++ date.js
@@ -0,0 +1,28 @@
+const today = new Date();
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const monthDay = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let day = weekday[today.getDay()];
+let month = monthDay[today.getMonth()];
+const day2 = today.getDate();
+document.getElementById("date").innerHTML = `${day}, ${month} ${day2}`;
